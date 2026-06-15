5f632bf4c21b1c0926bc293b7d62cfbde711a9a9
jagadeep <jagadeep2004@gmail.com>
2026-06-15 19:17:35 +0530

    new

--- restAssuredDemo/src/test/java/com/test/ApiValiidationTest.java
+++ restAssuredDemo/src/test/java/com/test/ApiValiidationTest.java
@@ -1,5 +1,19 @@
 package com.test;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
 public class ApiValiidationTest {
 
+	@Test
+	public void validateUserData() {
+		
+		String email = RestAssured.given().when().get("https://jsonplaceholder.typicode.com/users/1").jsonPath().getString("email");
+		
+		System.out.println("Email : "+email);
+		
+		Assert.assertEquals(email,"abc@gmail.com");
+	}
 }
